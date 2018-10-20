258d1c368096892a8d9239a2164d4ea2279ad32b
Ricardo <ricardobsb@outlook.com>
2018-10-19 22:05:23 -0300

    nova versão em 19Out2018

--- PA2_CODE.R
+++ PA2_CODE.R
@@ -1,7 +1,8 @@
-install.packages(c("data.table","R.utils"))
+install.packages(c("data.table","R.utils","dplyr"))
 
 library(R.utils)
 library(data.table)
+library(dplyr)
 
 # Downloading and unpacking from the source URL.
 if (!file.exists("repdata-data-StormData.csv.bz2")) 
@@ -12,15 +13,27 @@ if (!file.exists("repdata-data-StormData.csv"))
         bunzip2("repdata-data-StormData.csv.bz2", 
                 destname = "repdata-data-StormData.csv", remove = F)
 
-data <- read.csv("repdata-data-StormData.csv", stringsAsFactors = FALSE)
+data <- read.csv("repdata-data-StormData.csv", stringsAsFactors = FALSE,na.strings="NA")
 dim(data) # checking the dimensions of the data
 summary(data)
+colnames(data)
 
+head(data)
 # converting the format from the BGN_DATE variable to date
 data$BGN_DATE <- as.Date(data$BGN_DATE,"%m/%d/%Y") 
 
 # checking the EVTYPE  variable
 data$EVTYPE <- toupper(data$EVTYPE) # capitalizing all the events type
 
+# selecting some variables from original dataset
+data_clean <- subset(data, select = c("STATE__","EVTYPE","FATALITIES","INJURIES","PROPDMG","PROPDMGEXP","CROPDMG","CROPDMGEXP"))
+head(data_clean)
 
+# changing the variables names for a better comprehension
+names(data_clean) <- c("State", "Event", "Fatalities", "Injuries", "Property.Damage", "Property.Damage.Exp", "Crop.Damage", "Crop.Damage.Exp")
 
+# exploring the Event variable
+data_clean %>% 
+        group_by(Event) %>%
+        summarize(total=n()) %>% 
+        arrange(Event)
